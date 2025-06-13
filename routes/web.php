4868282f017f6845adbb81d1fd5093d941979cdb
Indah71 <indahjumiatin7@gmail.com>
2025-06-13 19:28:20 +0800

    update home

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function ()  {
+Route::get('/', function ()  {
     return view('app.home');
 })->name(name: 'home');
+
+Route::get('/home', function () {
+    return view('app.home');
+});
+
+Route::get('/about', function () {
+    return view('app.about');
+});
+
+Route::get('/content', function () {
+    return view('app.content');
+});
+
+Route::get('/food', function () {
+    return view('app.food');
+});
+
+Route::get('/forum', function () {
+    return view('app.forum');
+});
+
+Route::get('/recipe', function () {
+    return view('app.recipe');
+});
+
+Route::get('/calculator', function () {
+    return view('app.calculator');
+});
